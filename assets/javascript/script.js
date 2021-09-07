81de2e23c32e7ecd30816772535464f77b4b1e1d
Kondo <57133756+Kondo-Kazushi@users.noreply.github.com>
2021-09-07 12:58:32 +0900

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,10 @@
+const output = document.querySelector('#output');
 
+if(window.matchMedia('(prefers-color-scheme: dark)').matches === true){
+  output.innerHTML = "あなたは既にダークモードを使用しています";
+  
+}else{
+  (window.matchMedia('(prefers-color-scheme: dark)').matches === true){
+    output.innerHTML = "コントロールセンターからダークモードに変更してみてください";
+  }
+}
