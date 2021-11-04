ccf7a2790e77d0a9e73c3e401bc721e7d8295a2e
Kondo <57133756+Kondo-Kazushi@users.noreply.github.com>
2021-11-04 21:59:25 +0900

    Delete script.js

--- assets/javascript/script.js
+++
@@ -1,7 +0,0 @@
-const output = document.querySelector('#output');
-
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  output.innerHTML = "あなたは既にダークモードを使用しています";
-} else {
-    output.innerHTML = "コントロールセンターからダークモードに変更してみてください";
-}
